aeb2fc35c1b9d8dc2ab2c49fcbf5163fb7d9d5a0
jverneaut <jverneaut@gmail.com>
2019-08-13 00:58:23 +0200

    Add Facebook and Twitter

--- store/index.js
+++ store/index.js
@@ -2,15 +2,19 @@ import { writable, derived } from 'svelte/store';
 
 import Cross from '../components/svgs/Cross.svelte';
 import Menu from '../components/svgs/Menu.svelte';
+
 import ChevronLeft from '../components/svgs/ChevronLeft.svelte';
 import ChevronRight from '../components/svgs/ChevronRight.svelte';
 import ChevronTop from '../components/svgs/ChevronTop.svelte';
 import ChevronBottom from '../components/svgs/ChevronBottom.svelte';
 
+import Twitter from '../components/svgs/Twitter.svelte';
+import Facebook from '../components/svgs/Facebook.svelte';
 
 export const collections = [
   { name: 'Navigation', svgs: [Menu, Cross] },
   { name: 'Chevrons', svgs: [ChevronLeft, ChevronTop, ChevronRight, ChevronBottom] },
+  { name: 'Brands', svgs: [Twitter, Facebook] },
 ].map((collection, index, arr) => ({
   ...collection,
   baseIndex: arr.slice(0, index).reduce((acc, val) => acc.concat(val.svgs), []).length
