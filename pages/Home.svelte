35ad063f679fe404ed09c8050d457925760722cb
jverneaut <jverneaut@gmail.com>
2019-08-20 00:42:03 +0200

    Remove list from home page

--- pages/Home.svelte
+++ pages/Home.svelte
@@ -3,12 +3,5 @@
   import { Link } from "svelte-routing";
 </script>
 
-{#each collections as collection}
-  {collection}
-  <br />
-  {#each svgs.filter(svg => svg.collection === collection) as svg}
-    <Link to="/{svg.slug}">{svg.name}</Link>
-    <br />
-  {/each}
-  <br />
-{/each}
+<h1>This will be Inline Svg's landing page</h1>
+<Link to={svgs[0].slug}>Go to app</Link>
