ce12c0a157f45fb6ac702c570215d6eb7dc3e990
jverneaut <jverneaut@gmail.com>
2019-08-19 21:51:51 +0200

    Setup Svelte Routing

---
+++ App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { Router, Route, Link } from "svelte-routing";
+  import Home from "./pages/Home.svelte";
+  import Detail from "./pages/Detail.svelte";
+</script>
+
+<Router>
+  <Link to="/">Home</Link>
+  <Link to="/test">Detail</Link>
+  <div>
+    <Route path="/:svg" component={Detail} />
+    <Route path="/" component={Home} />
+  </div>
+</Router>
