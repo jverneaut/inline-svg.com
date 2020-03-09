07c40d3025f17aa224ca0fe11d94e83fd854ebfb
Julien Verneaut <jverneaut@gmail.com>
2020-03-09 14:04:56 +0100

    Add radisu on menu

--- components/svgs/Menu.svelte
+++ components/svgs/Menu.svelte
@@ -2,16 +2,25 @@
   export let width = 24;
   export let height = 16;
   export let thickness = 2;
-  export let color = '#ffffff';
+  export let color = "#ffffff";
+  export let radius = 0;
 
-  export const NAME = 'Menu';
-  export const TAGS = ['Menu', 'Hamburger'];
-  export const FILENAME = 'menu.svg';
+  export const NAME = "Menu";
+  export const TAGS = ["Menu", "Hamburger"];
+  export const FILENAME = "menu.svg";
 </script>
 
 <svelte:options accessors={true} />
 <svg fill={color} {width} {height} xmlns="http://www.w3.org/2000/svg">
-  <rect {width} height={thickness} />
-  <rect {width} height={thickness} y={height / 2 - thickness / 2} />
-  <rect {width} height={thickness} y={height - thickness} />
+  <rect {width} height={thickness} rx={radius === 0 ? null : radius} />
+  <rect
+    {width}
+    height={thickness}
+    y={height / 2 - thickness / 2}
+    rx={radius === 0 ? null : radius} />
+  <rect
+    {width}
+    height={thickness}
+    y={height - thickness}
+    rx={radius === 0 ? null : radius} />
 </svg>
